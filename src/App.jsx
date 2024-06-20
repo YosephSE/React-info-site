41b495e4eed72a1b2986817bafb9086a597012ae
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-06-20 04:46:35 +0300

    On progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import logo from "./image.png";
 function Header() {
   return (
     <div className="header">
+      <div className="logo-facts">
       <img className="logo" src={logo} alt="React-logo" />
+      <p>ReactFacts</p>
+      </div>
       <ul className="nav-list">
         <li>Home</li>
         <li>About</li>
@@ -14,9 +17,15 @@ function Header() {
 }
 function Main(){
   return(
-    <div>
-      <h1>Fun facts about react</h1>
-      
+    <div className="main">
+      <h1 className="main-header">Fun facts about react</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
     </div>
   )
 }
@@ -24,6 +33,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Main />
     </div>
   );
 }
